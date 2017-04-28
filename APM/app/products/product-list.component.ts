366f5b6baf27b50765636c22f92357e874aa5550
rosiehighsmith <rosie.highsmith@gmail.com>
2017-04-28 15:08:25 -0700

    added styleUrls and interface for strong typing

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
 	selector: 'pm-products',
-	templateUrl: 'app/products/product-list.component.html'
+	templateUrl: 'app/products/product-list.component.html',
+	styleUrls: ['app/products/product-list.component.css']
 })
 
 export class ProductListComponent {
@@ -11,14 +13,14 @@ export class ProductListComponent {
 	imageMargin: number = 2;
 	showImage: boolean = false;
 	listFilter: string = 'cart';
-	products: any[] = [
+	products: IProduct[] = [
 		{
 	        "productId": 2,
 	        "productName": "Garden Cart",
 	        "productCode": "GDN-0023",
 	        "releaseDate": "March 18, 2016",
 	        "description": "15 gallon capacity rolling garden cart",
-	        "price": 32.99,
+	        "price": 32.99 ,
 	        "starRating": 4.2,
 	        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
 	    },
