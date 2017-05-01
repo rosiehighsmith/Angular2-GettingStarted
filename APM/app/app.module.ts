558e1623c0d330d991a1fb19a45d6ad5b706d12e
rosiehighsmith <rosie.highsmith@gmail.com>
2017-05-01 08:00:15 -0700

    Nested star component inside product-list component

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { StarComponent } from './shared/star.component';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -14,7 +15,8 @@ import { ProductFilterPipe } from './products/product-filter.pipe';
   declarations: [ 
   	AppComponent,
   	ProductListComponent,
-  	ProductFilterPipe ],
+  	ProductFilterPipe,
+  	StarComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
