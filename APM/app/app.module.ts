592a07858a901960bcc332177294ee92165ae0d9
rosiehighsmith <rosie.highsmith@gmail.com>
2017-05-03 14:07:26 -0700

    start configuring routes for product detail and welcome pages

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { StarComponent } from './shared/star.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   imports: [ 
 	  BrowserModule,
-	  FormsModule
+	  FormsModule,
+    HttpModule,
+    RouterModule
   ],
   declarations: [ 
   	AppComponent,
+    WelcomeComponent,
   	ProductListComponent,
+    ProductDetailComponent,
   	ProductFilterPipe,
   	StarComponent ],
   bootstrap: [ AppComponent ]
