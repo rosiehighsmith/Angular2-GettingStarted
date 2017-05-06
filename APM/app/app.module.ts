cbe6d505a6202b13213826c241968ec4ba5b8c1b
rosiehighsmith <rosie.highsmith@gmail.com>
2017-05-06 16:41:22 -0700

    protected product detail routes with canActivate guards

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -6,7 +6,9 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-guard.service';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
@@ -18,7 +20,9 @@ import { StarComponent } from './shared/star.component';
     HttpModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'product/:id',
+        canActivate: [ ProductDetailGuard ],
+        component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
@@ -31,6 +35,7 @@ import { StarComponent } from './shared/star.component';
     ProductDetailComponent,
   	ProductFilterPipe,
   	StarComponent ],
+  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
