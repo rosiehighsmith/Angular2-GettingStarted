d8b2a7b0a4f1c9c072f562a226400472fcc56fc7
rosiehighsmith <rosie.highsmith@gmail.com>
2017-05-09 07:24:11 -0700

    added shared module

--- APM/app/products/product.module.ts
+++ APM/app/products/product.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductDetailGuard } from './product-guard.service';
-import { StarComponent } from '../shared/star.component';
 import { ProductService } from './product.service';
+
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [
 		ProductListComponent,
 		ProductDetailComponent,
-		ProductFilterPipe,
-		StarComponent
+		ProductFilterPipe
 	],
 	imports: [
-		FormsModule,
-		CommonModule,
+		SharedModule,
 		RouterModule.forChild([
 			{ path: 'products', component: ProductListComponent },
 		    { path: 'product/:id',
